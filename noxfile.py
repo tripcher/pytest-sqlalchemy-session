38b16cb07957dd7f0f3b830a7569924c3b4426d8
sshkitin <stanislav.shkitin@exness.com>
2023-04-09 15:13:47 +0300

    add postgres

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 @nox.parametrize("sqlalchemy_version", ["1.4"])
 @nox.parametrize("pytest_version", ["6", "7"])
 def tests(session: nox.Session, sqlalchemy_version: str, pytest_version: str):
