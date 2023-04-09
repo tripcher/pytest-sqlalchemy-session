42f16721555d7eeca627c6ad6337939c381afaea
sshkitin <stanislav.shkitin@exness.com>
2023-04-09 15:01:43 +0300

    fix env for tests

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,9 @@ import nox
 @nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 @nox.parametrize("sqlalchemy_version", ["1.4"])
 @nox.parametrize("pytest_version", ["6", "7"])
-def tests(session, sqlalchemy_version: str, pytest_version: str):
+def tests(session: nox.Session, sqlalchemy_version: str, pytest_version: str):
     session.install("-r", "requirements/base.txt")
     session.install(f"sqlalchemy=={sqlalchemy_version}")
     session.install(f"pytest=={pytest_version}")
     session.install("-r", "requirements/test.txt")
     session.run("pytest")
-
-
