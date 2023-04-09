07fb5a879004db7b4e2dacb920fbe151590adf70
sshkitin <stanislav.shkitin@exness.com>
2023-04-09 15:57:36 +0300

    fix python versions

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 @nox.parametrize("sqlalchemy_version", ["1.4"])
 @nox.parametrize("pytest_version", ["6.2.5", "7"])
 def tests(session: nox.Session, sqlalchemy_version: str, pytest_version: str):
